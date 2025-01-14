b4fd8c035fd0bc25f03ed9288573bbca8513c7a2
afienzidan <afienzidan227@gmail.com>
2025-01-14 20:35:55 +0700

    submit tugas6

---
+++ tugas-reactjs/src/App.1.js
@@ -0,0 +1,9 @@
+export function App() {
+  return (
+    <section id="section1">
+      <div className="card">
+        <h1> Hi, Saya Ahmad 'Afien Zidan </h1>
+      </div>
+    </section>
+  );
+}
---
+++ tugas-reactjs/src/App.js
@@ -0,0 +1,16 @@
+// import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <section id="section1">
+      <div className="card">
+        <h1> Hi, Saya Ahmad 'Afien Zidan 🧙‍♂️ </h1>
+        <hr></hr>
+        <p>Saya dari Batch 63 sudah berhasil menginstall React</p>
+      </div>
+    </section>
+  );
+}
+
+export default App;
